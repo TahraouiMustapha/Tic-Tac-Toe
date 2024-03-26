2f25e48b8c1521ce3d33516942834988635a5c73
mustapha <moustaphaoussama1818@gmail.com>
2024-03-26 03:01:42 +0100

    getting a working game in the console
    in this change, i added methods in Gameboard for :
    adding symbol X or O , check is game over, check
    is there winner.
    in gameController function , i added :
    startGame method for control the most of game,
    finishGame method for determine the winner in the
    console

--- script.js
+++ script.js
@@ -1,32 +1,97 @@
 const Gameboard = (function() {
-    let gameboard = [];
-    let index = 1;
-    
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++){
-            gameboard.push(cell(index++));
-        }
-    }
+    let gameboard = [
+        '','','',
+        '','','',
+        '','',''       
+    ];
+   
+    const getBoard = () => {
+        return gameboard;
+    };
 
-    return { gameboard };
+    const addSymbol = (symbol, index) => {
+        if (gameboard[index] === '') {
+            gameboard[index] = symbol;
+        }
+    };
+
+    const checkOver = () => {
+        return gameboard.every((element) => element != '');
+    };    
+
+    const checkWinner = (symbol) => {
+        let winnerLine = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],//for horizontal lines 
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],//for vertical lines 
+            [0, 4, 8], [2, 4, 6]//for diagonal lines 
+        ];
+
+        for (let line of winnerLine) {
+            if ( gameboard[line[0]] == symbol &&
+                 gameboard[line[1]] == symbol &&
+                 gameboard[line[2]] == symbol
+                 ) {
+                    return true;
+                 }
+        }
+
+        return false;
+    };
+
+    return { getBoard, addSymbol, checkOver, checkWinner };
 })();
 
-function createPlayer (name, assingnedXO) {
-    return {name, assingnedXO};
+function createPlayer (name, symbol) {
+    return {name, symbol};
 }
 
 const player1 = createPlayer('yassin', 'X');
 const player2 = createPlayer('moh', 'O');
 
 const gameController = (function () {
-    return {};
+    let turn = 'X';
+    const startGame = () => {
+        console.log(Gameboard.getBoard());
+        while(!Gameboard.checkOver()) {
+            console.log('player \''+ turn + '\' turn:');
+            let choice = prompt('enter your choice:');
+            if (turn === 'X') {        
+                Gameboard.addSymbol(turn, choice);
+                console.log(Gameboard.getBoard());
+                if (Gameboard.checkWinner(turn)) {
+                    finishGame(turn);
+                    break;
+                }
+                turn = 'O';
+            } else {
+                Gameboard.addSymbol(turn, choice);
+                console.log(Gameboard.getBoard());
+                if (Gameboard.checkWinner(turn)) {
+                    finishGame(turn);
+                    break;
+                }
+                turn = 'X';
+            }
+        }
+    };
+
+    const finishGame = (symbolWon) => {
+        console.log('good job');
+        let name;
+        if (symbolWon === player1.symbol) {
+            name = player1.name;
+        } else {
+            name = player2.name;
+        }
+        console.log(name + ' you are win with ' + symbolWon);
+    };
+
+    return { startGame, finishGame };
 })();
 
 const displayController = (function () {
     return {}; 
 })();
 
-//object Cell for each gameboard
-function cell (index) {
-    return { index };
-} 
+
+gameController.startGame();
